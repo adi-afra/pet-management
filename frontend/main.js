f9f770bf2e68daf8998fbf8eacb2bd318232e434
Najifa Tahsin <najifatahsin@Mac.Home>
2026-02-06 12:57:07 +0000

    Add new folder with initial files

---
+++ frontend/main.js
@@ -0,0 +1,16 @@
+// Select all filter buttons
+const filterButtons = document.querySelectorAll('.filter-btn');
+
+// Add click event to each button
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        // Remove active class from all buttons
+        filterButtons.forEach(btn => {
+            btn.classList.remove('active');
+        });
+
+        // Add active class to the clicked button
+        button.classList.add('active');
+    });
+});
